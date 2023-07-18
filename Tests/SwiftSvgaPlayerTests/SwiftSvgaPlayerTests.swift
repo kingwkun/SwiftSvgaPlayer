37a803fdd38e2fa83908531674342894782b7288
kerwin <kerwin@miaoshitech.com>
2023-07-18 17:03:19 +0800

    init

---
+++ Tests/SwiftSvgaPlayerTests/SwiftSvgaPlayerTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import SwiftSvgaPlayer
+@testable import SwiftSvgaPlayer
+
+final class SwiftSvgaPlayerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftSvgaPlayer().text, "Hello, World!")
+        let svga = SwiftSvgaPlayer()
+    }
+}
